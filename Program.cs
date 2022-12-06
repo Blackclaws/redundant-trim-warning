7dbdb1defaaf7e63381ec540e0d10f34870c8d28
Felix Winterhalter <felix@arfinity.io>
2022-12-06 14:44:34 +0100

    feat: Show Issue

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+
+Test.MethodOne<Program>();
+
+public static class Test
+{
+    
+    public static void MethodOne<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]T>()
+    {
+        var variableRequiringClosure = "value";
+        async Task LocalFunction()
+        {
+            Console.WriteLine(variableRequiringClosure);
+            await Task.CompletedTask;
+        }
+        
+        // Suppress async warning
+#pragma warning disable CS4014
+        LocalFunction();
+#pragma warning restore CS4014
+    }
+}
